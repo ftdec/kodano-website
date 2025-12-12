87828d728113ea58c7e4fe23ead523aef891c16e
FlucsDeveloper <developer@flucs.com.br>
2025-12-12 04:53:26 -0300

    fix(build): resolve TypeScript error in PaymentVision scrollProgress prop

--- src/components/home/payment-vision/KodanoPaymentVision.tsx
+++ src/components/home/payment-vision/KodanoPaymentVision.tsx
@@ -89,7 +89,7 @@ export function KodanoPaymentVision({
           {/* Main scene */}
           <PaymentVisionScene
             isMobile={isMobile}
-            scrollProgress={scrollProgress}
+            scrollProgress={scrollProgress ?? null}
           />
         </Canvas>
       </Suspense>
